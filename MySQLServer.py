2de37d1656d94085566bcfedbb8b61a2f70acaaf
Cate <catherinenzisa2017@gmail.com>
2025-06-28 23:01:38 +0300

    Add MySQLServer.py file

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,10 +1,12 @@
 import mysql.connector
+
+"CREATE DATABASE IF NOT EXISTS alx_book_store;"
+
 try:
     mydb = mysql.connector.connect(
         host="localhost",
         user="root",
         password="Nzisa@1999pujo",
-        database="alx_book_store"
     )
 
     mycursor = mydb.cursor()
