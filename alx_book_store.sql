0b4e1fff472521b237ec5423188a7fd016ca3831
Cate <catherinenzisa2017@gmail.com>
2025-06-28 22:36:15 +0300

    Update alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_id INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id INT NOT NULL,
-    FOREIGN KEY(book_id) REFERENCES Books(book_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
     quantity DOUBLE
 );
 
