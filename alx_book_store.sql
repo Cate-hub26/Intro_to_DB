37f6238bd026da01be3e55a119e25ae1976e9fb3
Cate <catherinenzisa2017@gmail.com>
2025-06-28 22:30:45 +0300

    Update alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,20 +1,20 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
-CREATE TABLE Authors (
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email UNIQUE NOT NULL,
     address TEXT
 );
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT NOT NULL,
@@ -23,14 +23,14 @@ CREATE TABLE Books (
     publication_date DATE
 );
 
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
     FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
-CREATE TABLE Order_Details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT NOT NULL,
     FOREIGN KEY(order_id) REFERENCES Orders(order_id),
